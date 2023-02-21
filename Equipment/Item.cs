b24cf1192b2a27a3112bad25e1f75541139fb0d8
Daan Daling <73099939+DaanDaling@users.noreply.github.com>
2023-02-21 20:46:30 +0100

    [WIP] Objects
    Setup for loot object
    still need to add enum

--- Equipment/Item.cs
+++ Equipment/Item.cs
@@ -8,5 +8,14 @@ namespace Threading_in_C.Equipment
 {
     internal class Item
     {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Rarity { get; set; }
+        public int Value { get; set; }
+        public string Description { get; set; }
+        public List<string> Properties { get; set; }
+        public List<string> Drawbacks { get; set; }
+        public List<string> Requirements { get; set; }
+        public string History { get; set; }
     }
 }
