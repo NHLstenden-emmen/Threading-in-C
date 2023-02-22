47e71543328615c6f7e6059dbc3d6b74bf9d477c
kevin smulders <39293162+kevinsmul@users.noreply.github.com>
2023-02-22 12:18:32 +0100

    Update PlayerScreenForm.cs

--- Forms/PlayerScreenForm.cs
+++ Forms/PlayerScreenForm.cs
@@ -47,7 +47,7 @@ namespace Threading_in_C.Forms
             }
             else
             {
-                var player = new Player();
+                var player = new Player(PlayerNameTextbox.Text, (int)PlayerHealthNumeric.Value, (int)PlayerMovementNumeric.Value, (int)PlayerStrengthNumeric.Value, (int)PlayerDexterityNumeric.Value, (int)
                 player.Name = PlayerNameTextbox.Text;
                 player.Health = (int)PlayerHealthNumeric.Value;
                 player.Movement = (int)PlayerMovementNumeric.Value;
