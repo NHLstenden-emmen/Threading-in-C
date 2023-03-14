fc74278ef10f5f9af7e8c37f7a5022e9bb1bcba7
dreddyaell <72259796+dreddyaell@users.noreply.github.com>
2023-03-14 18:44:52 +0100

    CalculatorCR
    character en monster CR

---
+++ CRCalculator/CRMonster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading_in_C.CRCalculator
+{
+    internal class CRMonster
+    {
+    }
+}
