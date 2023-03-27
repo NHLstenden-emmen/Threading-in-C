047dce9c776420a0400a07934abc46756cbfa426
RoanMeijer <Roan.meijer10@gmail.com>
2023-03-27 15:27:50 +0200

    Movable and Inmobvable
    created abstract classes for inmovable and movable placables

---
+++ Board/placeable/InMovable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading_in_C.Board.placeable
+{
+    internal abstract class InMovable : Placeable
+    {
+        protected InMovable() { }
+     
+    }
+}
