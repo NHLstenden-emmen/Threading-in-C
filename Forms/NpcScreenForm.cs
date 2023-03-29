904dfda07fdb143616ecfdf1944097bad2917574
kevin smulders <39293162+kevinsmul@users.noreply.github.com>
2023-03-29 19:28:44 +0200

    setup basic retrieve of npc

--- Forms/NpcScreenForm.cs
+++ Forms/NpcScreenForm.cs
@@ -12,9 +12,58 @@ namespace Threading_in_C.Forms
 {
     public partial class NpcScreenForm : Form
     {
+        private List<NPC> npcs = new List<NPC>();
+        private ManualResetEvent threadExitEvent = new ManualResetEvent(false);
+        private int numThreads = 0;
         public NpcScreenForm()
         {
             InitializeComponent();
+        // Retrieves all the npcs from the db and puts them in the npcs list
+        private void RetrieveNpcsFromDatabase()
+        {
+            OpenFiveApiRequest.con.Open();
+            npcs.Clear();
+            SavedNpcsListBox.Items.Clear();
+
+            string retrieveSQL = "SELECT * FROM NPCs";
+            using (SqlCommand command = new SqlCommand(retrieveSQL, OpenFiveApiRequest.con))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        List<string> traits = new List<string>();
+                        string[] traitStrings = reader["Traits"].ToString().Split(';');
+                        foreach (string traitString in traitStrings)
+                        {
+                            traits.Add(traitString);
+                        }
+
+                        NPC npc = new NPC
+                        (
+                            reader["Name"].ToString(),
+                            (int)reader["Health"],
+                            (int)reader["Movement"],
+                            (int)reader["Strength"],
+                            (int)reader["Dexterity"],
+                            (int)reader["Constitution"],
+                            (int)reader["Intelligence"],
+                            (int)reader["Wisdom"],
+                            (int)reader["Charisma"],
+                            (int)reader["ArmorRating"],
+                            (int)reader["Proficiency"],
+                            reader["Race"].ToString(),
+                            reader["Class"].ToString(),
+                            reader["Backstory"].ToString(),
+                            traits
+                        );
+                        npcs.Add(npc);
+                    }
+                }
+            }
+
+            OpenFiveApiRequest.con.Close();
+        }
         }
     }
 }
