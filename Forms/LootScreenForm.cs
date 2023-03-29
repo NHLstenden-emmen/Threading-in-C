9c859cb1e5fb015b5fb4c4249a7796c79cefd251
kevin smulders <39293162+kevinsmul@users.noreply.github.com>
2023-03-29 20:09:52 +0200

    setup for use of the Items/loot

--- Forms/LootScreenForm.cs
+++ Forms/LootScreenForm.cs
@@ -2,19 +2,50 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Threading_in_C.ApiGenerators;
+using Threading_in_C.ApiResponseAdapters;
+using Threading_in_C.Entities;
+using Threading_in_C.Equipment;
+using Threading_in_C.OpenFiveApi;
 
 namespace Threading_in_C.Forms
 {
     public partial class LootScreenForm : Form
     {
+        private List<Item> items = new List<Item>();
+        private ManualResetEvent threadExitEvent = new ManualResetEvent(false);
+        private int numThreads = 0;
+        private Mutex dbMutex = new Mutex();
+
         public LootScreenForm()
         {
             InitializeComponent();
+            RetrieveItemsFromDatabase();
+            AddItemsToList();
+        }
+
+        // Displays all the items in the items list in the list box
+        private void AddItemsToList()
+        {
+            foreach (Item item in items)
+            {
+                // Add the player to the ListBox control
+                SavedItemsListBox.Items.Add(item.Name.ToString());
+            }
+        }
+
+        private void RefreshSavedItems_Click(object sender, EventArgs e)
+        {
+            RetrieveItemsFromDatabase();
+            AddItemsToList();
+        }
         }
     }
 }
